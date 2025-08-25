85643e3f8e8e688f8d19aa8467379b68e9f2668c
Joshua Knauber <joshua.knauber@gmail.com>
2025-08-26 00:18:02 +0200

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { requestId } from "hono/request-id";
 import * as path from "path";
 
 const app = new Hono();
 
+app.use("*", cors());
+app.use(logger());
+app.use("*", requestId());
+
 app.use(
   "*",
   basicAuth({
-    username: "jknauber",
-    password: "madeforpaper",
+    username: process.env.PAGE_USERNAME!,
+    password: process.env.PAGE_PASSWORD!,
   })
 );
 
