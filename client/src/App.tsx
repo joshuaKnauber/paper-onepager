7f63a3a4bbdfccee891d3b4fd86108503cc96265
Joshua Knauber <joshua.knauber@gmail.com>
2025-08-27 13:15:45 +0200

    structure & shader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,38 @@
+import { ditheringFragmentShader, ShaderMount } from "@paper-design/shaders";
 import { DotGrid } from "@paper-design/shaders-react";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
+
 import { Canvas } from "./components/Canvas";
 import page from "./content.html?raw";
 
 export function App() {
   const [history, setHistory] = useState<string[]>([page]);
   const currentState = history[history.length - 1] || "";
+
+  useLayoutEffect(() => {
+    const editContainer = document.getElementById("edit-cta");
+    if (!editContainer) return;
+    const hasCanvas = editContainer.querySelector("canvas");
+    if (hasCanvas) return;
+
+    new ShaderMount(
+      editContainer,
+      ditheringFragmentShader,
+      {
+        u_colorBack: [0.9375, 0.933, 0.917, 1.0],
+        u_colorFront: [0.804, 0.792, 0.746, 1.0],
+        u_shape: 0.0,
+        u_type: 1.0,
+        u_pxSize: 9.0,
+        u_offsetX: 0.0,
+        u_offsetY: 0.0,
+        u_scale: 1.0,
+        u_rotation: 0.0,
+      },
+      undefined,
+      1.0
+    );
+  }, []);
 
   return (
     <>
